ec66066ff59f960327849d0fa6fd2a84930b54e0
igustavo11 <gustavobrun1224@gmail.com>
2025-10-26 23:18:00 -0300

    Add Vercel configuration file for deployment settings

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,11 +6,6 @@ export default defineConfig({
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    host: env.DB_HOST,
-    port: env.DB_PORT,
-    user: env.DB_USER,
-    password: env.DB_PASSWORD,
-    database: env.DB_NAME,
-    ssl: false,
+    url: env.DATABASE_URL,
   },
 });
